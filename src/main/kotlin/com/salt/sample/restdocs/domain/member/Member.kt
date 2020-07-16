77ffecc8bb56a1a2bbdc9ea63160ce837767fbd4
salt <salt@socar.kr>
2020-07-16 18:02:45 +0900

    testcase 작성 및 RestDocs 적용

--- src/main/kotlin/com/salt/sample/restdocs/domain/member/Member.kt
+++ src/main/kotlin/com/salt/sample/restdocs/domain/member/Member.kt
@@ -4,16 +4,19 @@ import com.salt.sample.restdocs.domain.base.BaseEntity
 import com.salt.sample.restdocs.dto.MemberBody
 import java.time.LocalDate
 import javax.persistence.Entity
+import javax.persistence.Id
 import javax.persistence.Table
 
 @Entity
 @Table(name = Member.TABLE_NAME)
 class Member(memberBody: MemberBody) : BaseEntity() {
 
+    @Id
+    var id: Long = memberBody.id
     val name: String? = memberBody.name
     val joinDate: LocalDate? = memberBody.joinDate
 
     companion object {
-        const val TABLE_NAME = "member"
+        const val TABLE_NAME = "member_info"
     }
 }
